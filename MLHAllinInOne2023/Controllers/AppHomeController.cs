75a1016d444d979b5956b5761794c23a30f91710
Albert Luna <luna.alvr1@gmail.com>
2023-03-25 20:54:47 -0500

    commit before refactor

---
+++ MLHAllinInOne2023/Controllers/AppHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MLHAllinInOne2023.Controllers
+{
+    public class AppHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MLHAllinInOne2023/Pages/AppHome.cshtml.cs
@@ -0,0 +1,26 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Runtime.CompilerServices;
+using System.Xml.Linq;
+
+namespace MLHAllinInOne2023.Pages
+{
+    public class AppHomeModel : PageModel
+    {
+        [BindProperty]
+        public string otherIntrests{get; set;}
+        public string[] intrests = ["art", "food", "nature", "nightlife", "music"];
+        public bool ShowForm { get; set; } //get request = true, post request = false
+
+        public void OnGet()
+        {
+            ShowForm = true;
+        }
+        
+        public void OnPost()
+        {
+            ShowForm = false;
+            ViewData["Message"] = "Other Intrests: " + otherIntrests;
+        }
+    }
+}
