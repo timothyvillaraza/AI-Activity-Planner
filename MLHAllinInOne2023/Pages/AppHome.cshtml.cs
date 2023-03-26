7ad90ff8badac8981baf865d1b50a9005fad9419
Albert Luna <luna.alvr1@gmail.com>
2023-03-25 21:42:57 -0500

    booleans integrated

--- MLHAllinInOne2023/Pages/AppHome.cshtml.cs
+++ MLHAllinInOne2023/Pages/AppHome.cshtml.cs
@@ -6,21 +6,40 @@ using System.Xml.Linq;
 namespace MLHAllinInOne2023.Pages
 {
     public class AppHomeModel : PageModel
-    {
+    { 
+        // checkboxes display on page 
+        public string[] intrests = {
+            "art",
+            "food",
+            "nature",
+            "nightlife",
+            "music"
+        };
+
         [BindProperty]
         public string otherIntrests { get; set; }
-        public string[] intrests = { "art", "food", "nature", "nightlife", "music" };
-        public bool ShowForm { get; set; }//get request = true, post request = false
+        [BindProperty]
+        public bool art { get; set; }
+        [BindProperty]
+        public bool food { get; set; }
+        [BindProperty]
+        public bool nature { get; set; }
+        [BindProperty]
+        public bool nightlife { get; set; }
+        [BindProperty]
+        public bool music { get; set; }
+
+        public bool ShowForm { get; set; } // get request = true, post request = false
 
         public void OnGet()
         {
             ShowForm = true;
         }
-        
+
         public void OnPost()
         {
             ShowForm = false;
-            ViewData["Message"] = "Other Intrests: " + otherIntrests;
+            ViewData["Message"] = "Other Intrests: " + otherIntrests + " " + art;
         }
     }
 }
